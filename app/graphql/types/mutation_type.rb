d03f0e1c9685785867dc346aedd649414f0b6db7
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-12-03 21:42:49 -0700

    feat: delete user query working

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :destroy_user, mutation: Mutations::DestroyUser
     field :create_user, mutation: Mutations::CreateUser
   end
 end
