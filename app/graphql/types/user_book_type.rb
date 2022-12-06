0ef678139e4bd86a0351561d2d64a8a239bf7af7
Jake Kim <imjakekim@gmail.com>
2022-12-05 16:45:07 -0800

    added book method to userbooktype.rb

--- app/graphql/types/user_book_type.rb
+++ app/graphql/types/user_book_type.rb
@@ -4,8 +4,13 @@ module Types
   class UserBookType < Types::BaseObject
     field :id, ID, null: false
     field :user_id, Integer
-    field :book_id, Integer
+    field :book_id, ID
     field :status, Integer
-    field :books, [Types::BookType]
+    # field :books, [Types::BookType]
+    field :book, Types::BookType
+
+    def book
+      Book.find(object.book_id)
+    end
   end
 end
