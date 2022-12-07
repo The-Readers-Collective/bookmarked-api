278cba153d437518739129342c6c9610fe31acf2
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-12-07 10:14:22 -0700

    feat: updated QueryType to now accept books and book(id), query succesful in GraphiQL

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -18,5 +18,19 @@ module Types
     def user(id:)
       User.find(id)
     end
+
+    field :books, [Types::BookType], null: false
+
+    def books
+      Book.all
+    end
+
+    field :book, Types::BookType,  null: false do
+      argument :id, ID, required: true
+    end
+
+    def book(id:)
+      Book.find(id)
+    end
   end
 end
