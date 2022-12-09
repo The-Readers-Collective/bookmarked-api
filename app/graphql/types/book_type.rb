b02cb53ec96fc06557025a222cddc849feb573c4
Jake Kim <imjakekim@gmail.com>
2022-12-09 15:05:14 -0800

    implemented queries for getting books with same google book id

--- app/graphql/types/book_type.rb
+++ app/graphql/types/book_type.rb
@@ -15,6 +15,8 @@ module Types
     field :available, Boolean
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-    
+
+
+
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,9 +4,7 @@ module Types
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
-
     field :users, [Types::UserType], null: false
-
     def users
       User.all
     end
@@ -14,13 +12,11 @@ module Types
     field :user, Types::UserType, null: false do
       argument :id, ID, required: true
     end
-
     def user(id:)
       User.find(id)
     end
 
     field :books, [Types::BookType], null: false
-
     def books
       Book.all
     end
@@ -28,7 +24,6 @@ module Types
     field :book, Types::BookType, null: false do
       argument :id, ID, required: true
     end
-
     def book(id:)
       Book.find(id)
     end
@@ -36,13 +31,11 @@ module Types
     field :google_books, [Types::GoogleBookType], null: false do
       argument :title, String, required: true
     end
-
     def google_books(title:)
       GoogleBooksFacade.books_by_name(title)
     end
 
     field :user_books, [Types::UserBookType], null: false
-
     def user_books
       UserBook.all
     end
@@ -50,9 +43,15 @@ module Types
     field :user_book, Types::UserBookType, null: false do
       argument :id, ID, required: true
     end
-
     def user_book(id:)
       UserBook.find(id)
+    end
+
+    field :books_by_google_book_id, [Types::BookType], null: false do
+      argument :google_book_id, String, required: true
+    end
+    def books_by_google_book_id(google_book_id:)
+      Book.where(google_book_id: google_book_id)
     end
 
   end
