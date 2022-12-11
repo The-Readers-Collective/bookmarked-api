9403581ba2e8dff6903ae8049a63fa9fdf4710a1
Sunny Moore <moore.sunny@gmail.com>
2022-12-11 11:21:03 -0800

    Update the update mutations to required: false

--- app/graphql/types/user_attributes.rb
+++ app/graphql/types/user_attributes.rb
@@ -1,8 +1,8 @@
 module Types
   class UserAttributes < BaseInputObject
     description "Attributes for creating or updating a User"
-    argument :name, String
-    argument :zipcode, String
-    argument :email, String
+    argument :name, String, required: false
+    argument :zipcode, String, required: false
+    argument :email, String, required: false
   end
 end
