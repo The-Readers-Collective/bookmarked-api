75e3f3d5118e5801da697dca95c01169f38a8f6f
Sunny Moore <moore.sunny@gmail.com>
2022-12-12 12:34:23 -0800

    Add validation of status to user_books and test those

--- spec/models/user_book_spec.rb
+++ spec/models/user_book_spec.rb
@@ -5,4 +5,7 @@ RSpec.describe UserBook, type: :model do
     it { should belong_to :user }
     it { should belong_to :book }
   end
+  describe 'validations' do
+    it {should validate_presence_of :status}
+  end
 end
