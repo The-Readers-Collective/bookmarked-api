6effb6798bdb922e01ce7a9d0806cda0d8c08dc0
Sunny Moore <moore.sunny@gmail.com>
2022-12-15 09:37:41 -0800

    Added validations and tests to books. could not add validation for boolean available field

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,7 @@ class Book < ApplicationRecord
   enum condition: { POOR: 0, GOOD: 1, EXCELLENT: 2 }
   has_many :user_books
   has_many :users, through: :user_books
+
+  validates_presence_of :google_book_id, :book_title, :author,  :isbn_13, :condition
+  # validates :available, inclusion: [true, false]
 end
